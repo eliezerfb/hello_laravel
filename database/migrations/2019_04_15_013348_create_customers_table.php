f556a16ec2645efc38ec1480c5cbe3b0705c156b
Eliézer Bourchardt <eliezerfb@gmail.com>
2019-05-04 10:20:14 -0300

    change customers

--- database/migrations/2019_04_15_013348_create_customers_table.php
+++ database/migrations/2019_04_15_013348_create_customers_table.php
@@ -15,8 +15,13 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('phone');
+            $table->string('name', 100);
+            $table->string('cnpj_cpf', 14);
+            $table->string('phone', 20);
+            $table->date('birthdate');
+            $table->string('address', 150);
+            $table->string('city', 100);
+            $table->string('state', 2);
             $table->timestamps();
         });
     }
